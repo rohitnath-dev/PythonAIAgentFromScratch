c4545324cdd03a3e8bce0b2c82f842fdb47a628e
Rohit Nath <rohitnath49195@gmail.com>
2026-06-29 16:01:51 +0530

    update tool initialization

--- tools.py
+++ tools.py
@@ -3,6 +3,21 @@ from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.tools import Tool
 from datetime import datetime
 
+search = DuckDuckGoSearchRun()
+
+search_tool = Tool(
+    name="search",
+    func=search.run,
+    description="Search the web for general product information.",
+)
+
+api_wrapper = WikipediaAPIWrapper(
+    top_k_results=1,
+    doc_content_chars_max=300,
+)
+
+wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
+
 def save_to_txt(data: str, filename: str = "research_output.txt"):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     formatted_text = f"--- Research Output ---\nTimestamp: {timestamp}\n\n{data}\n\n"
