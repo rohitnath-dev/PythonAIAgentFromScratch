0665327d9b2dc790276e6c5a4f6cb68cdda6e0ca
Rohit Nath <rohitnath49195@gmail.com>
2026-06-29 15:53:32 +0530

    add shopping tools

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
-from tools import search_tool, wiki_tool, save_tool
+from tools import (
+    search_tool,
+    reddit_tool,
+    youtube_tool,
+    price_tool,
+    coupon_tool,
+    expert_review_tool,
+    compare_tool,
+    wiki_tool,
+    save_tool,
+)
 
 load_dotenv()
 
@@ -107,7 +117,17 @@ Return ONLY the JSON format specified below.
     format_instructions=parser.get_format_instructions()
 )
 
-tools = [search_tool, wiki_tool, save_tool]
+tools = [
+    search_tool,
+    reddit_tool,
+    youtube_tool,
+    price_tool,
+    coupon_tool,
+    expert_review_tool,
+    compare_tool,
+    wiki_tool,
+    save_tool,
+]
 agent = create_tool_calling_agent(
     llm=llm,
     prompt=prompt,
